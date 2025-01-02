05e59626379164f5cc767fd8e083149f5302262e
fllesser <fllessive@gmail.com>
2025-01-03 03:36:31 +0800

    添加清空羡慕和当前羡慕命令

--- nonebot_plugin_envious/__int__.py
+++ nonebot_plugin_envious/__int__.py
@@ -24,8 +24,8 @@ import nonebot_plugin_localstore as store
 
 __plugin_meta__ = PluginMetadata(
     name="羡慕 koishi",
-    description="羡慕 koishi",
-    usage="羡慕",
+    description="复读羡慕，并收纳关键词，自动羡慕",
+    usage="羡慕xxx/清空羡慕/当前羡慕",
     type="application",
     homepage="https://github.com/fllesser/nonebot-plugin-envious",
     supported_adapters={ "~onebot.v11" }
@@ -46,6 +46,7 @@ async def _():
         keywords_file.write_text(json.dumps(["koishi"]))
     global keyword_set
     keyword_set = set(json.loads(keywords_file.read_text()))
+    logger.info(f"羡慕: {keyword_set}")
 
 def save_keywords():
     keywords_file: Path = store.get_plugin_data_file("keywords.json")
@@ -66,11 +67,21 @@ def contains_keywords(event: MessageEvent, state: T_State) -> bool:
 
 
 envious = on_message(
-    rule = contains_keywords
+    rule = contains_keywords,
+    priority = 1027
 )
 
 add_keywords = on_command(
-    cmd = '羡慕'
+    cmd = '羡慕',
+    block = True
+)
+
+clear_envious = on_command(
+    cmd = '清空羡慕'
+)
+
+list_envious = on_command(
+    cmd = '当前羡慕'
 )
 
 @envious.handle()
@@ -95,9 +106,9 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     if not keyword or '羡慕' in keyword or keyword == last_envious.get(gid):
         return
     if len(keyword) > MAX_LEN and (match := re.search(r'[0-9A-Za-z]+', keyword)):
-        keyword = match.group()
+        keyword = match.group(0)
     if len(keyword) > MAX_LEN:
-        return
+        await add_keywords.finish("你在瞎羡慕什么呢？")
     
     lock = locks.get(gid)
     if not lock:
@@ -109,4 +120,17 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     keyword_set.add(keyword)
     save_keywords()
     await add_keywords.send("羡慕" + keyword)
+
+@clear_envious.handle()
+async def _():
+    keyword_set.clear()
+    save_keywords()
+    await clear_envious.send("哼(`3´)，我啥也不会羡慕了")
     
+@list_envious.handle()
+async def _():
+    if keywords_str := '、'.join(keyword_set):
+        res = "我现在巨tm羡慕" + keywords_str
+    else:
+        res = "不好意思，我啥也不羡慕"
+    await list_envious.send(res)
