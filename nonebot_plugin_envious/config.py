e8f3bb8aa70d8c365e4295628030ec98c6d3d007
fllesser <fllessive@gmail.com>
2025-03-10 23:06:23 +0800

    🦄 refactor: 小重构

--- nonebot_plugin_envious/config.py
+++ nonebot_plugin_envious/config.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel, confloat
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+
 
 class Config(BaseModel):
     envious_max_len: int = 10
-    envious_probability: confloat(ge=0.0, le=1.0) = 0.7
-    envious_list: list[str] = ['koishi']
-    
+    envious_probability: Annotated[float, Field(ge=0.0, le=1.0)] = 0.7
+    envious_list: list[str] = ["koishi"]
