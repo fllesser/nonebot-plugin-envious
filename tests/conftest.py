b9b6d56ed9815f2454fb0c6a4996bbadf8682f11
fllesser <fllessive@gmail.com>
2025-09-14 13:45:16 +0800

    🧪 test: ...

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,10 +23,3 @@ async def after_nonebot_init(after_nonebot_init: None):
 
     # 加载插件
     nonebot.load_from_toml("pyproject.toml")
-
-
-@pytest.fixture(scope="function", autouse=True)  # noqa: PT003
-async def _():
-    from nonebot_plugin_envious import gem
-
-    await gem.reset()
